27f247545d5b68742c4d5993feffe0f5d5d0cd08
Sivabudh Umpudh <sivabudh.u@codium.co>
2017-03-18 23:05:51 +0700

    Part 1 is complete.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Todo} from "./todo";
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.createTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    this.todoDataService.toggleTodoComplete(todo.id);
+  }
+
+  removeTodo(todo: Todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
+
+
